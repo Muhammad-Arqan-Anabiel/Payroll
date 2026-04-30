f6b13319b22367fc61ea23bd0bb6b746a2336881
unknown <lilaharyanti45@gmail.com>
2026-04-30 11:29:24 +0700

    update 2

---
+++ app/Filament/Resources/Schedules/Schemas/ScheduleForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Schedules\Schemas;
+
+    use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+
+class ScheduleForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Group::make()->components([
+                    Section::make()->components([
+                        Select::make('user_id')
+                            ->label('Nama Pegawai')
+                            ->relationship('user', 'name')
+                            ->preload()
+                            ->searchable()
+                            ->required(),
+                        Select::make('shift_id')
+                            ->relationship('shift', 'name')
+                            ->required(),
+                        Select::make('office_id')
+                            ->relationship('office', 'name')
+                            ->required(),
+                        Toggle::make('is_wfa')
+                            ->label('WFA')
+                            ->onIcon(Heroicon::Home)
+                            ->offIcon(Heroicon::BuildingOffice2),
+                    ])
+                ]),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Schedules/Tables/SchedulesTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\Schedules\Tables;
+
+use App\Models\Schedule;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SchedulesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->label('Nama Pegawai')
+                    ->searchable(),
+                BooleanColumn::make('is_wfa')
+                    ->label('WFA'),
+                TextColumn::make('shift.name')
+                    ->description(fn (Schedule $schedule) => $schedule->shift->start_time . ' - ' . $schedule->shift->end_time)
+                    ->searchable(),
+                TextColumn::make('office.name')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
